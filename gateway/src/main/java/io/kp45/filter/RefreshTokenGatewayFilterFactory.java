eec6ba2e1c1424524e7313d4178973d213763ca3
ReLive27 <61924393+ReLive27@users.noreply.github.com>
2022-06-07 00:07:34 +0800

    Add Gateway Project (#14)
    * Add OAuth2 Authorization Server
    
    * Add Gateway Project
    
    Co-authored-by: relive <relive27@163.com>

---
+++ gateway/src/main/java/io/kp45/filter/RefreshTokenGatewayFilterFactory.java
@@ -0,0 +1,105 @@
+package io.kp45.filter;
+
+import io.kp45.filter.conver.RefreshTokenAuthenticationConverter;
+import io.kp45.filter.conver.ServerAuthenticationConverter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ReactiveHttpInputMessage;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.endpoint.OAuth2ParameterNames;
+import org.springframework.stereotype.Component;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.reactive.function.BodyExtractor;
+import org.springframework.web.reactive.function.BodyExtractors;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.client.ClientResponse;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.util.Map;
+
+@Component
+@RequiredArgsConstructor
+public class RefreshTokenGatewayFilterFactory extends AbstractGatewayFilterFactory<Object> {
+    private static final String REFRESH_TOKEN_VALUE = "refreshToken";
+    private static String ACCESS_TOKEN_COOKIE_NAME = "ACCESS-TOKEN";
+    private static String REFRESH_TOKEN_COOKIE_NAME = "REFRESH-TOKEN";
+    private static final ParameterizedTypeReference<Map<String, Object>> STRING_OBJECT_MAP = new ParameterizedTypeReference<Map<String, Object>>() {
+    };
+    private BodyExtractor<Mono<Map<String, Object>>, ReactiveHttpInputMessage> bodyExtractor = new BodyExtractor<Mono<Map<String, Object>>, ReactiveHttpInputMessage>() {
+        @Override
+        public Mono<Map<String, Object>> extract(ReactiveHttpInputMessage inputMessage, Context context) {
+            BodyExtractor<Mono<Map<String, Object>>, ReactiveHttpInputMessage> delegate = BodyExtractors
+                    .toMono(STRING_OBJECT_MAP);
+            return delegate.extract(inputMessage, context)
+                    .onErrorMap((ex) -> new OAuth2AuthenticationException("An error occurred parsing the Access Token response: " + ex.getMessage()))
+                    .switchIfEmpty(Mono.error(() -> new OAuth2AuthenticationException("Request access token response is empty")));
+
+        }
+    };
+    private ServerAuthenticationConverter serverAuthenticationConverter = new RefreshTokenAuthenticationConverter();
+    private final WebClient loadWebClient;
+
+    @Override
+    public GatewayFilter apply(Object config) {
+        return (exchange, chain) -> {
+            if (exchange.getAttributeOrDefault(REFRESH_TOKEN_VALUE, false)) {
+                return this.serverAuthenticationConverter.convert(exchange)
+                        .flatMap((refreshTokenValue) -> this.loadWebClient.post()
+                                .uri("http://iam-server/iam/oauth2/token")
+                                .body(createTokenRequestBody(refreshTokenValue))
+                                .exchange()
+                                .flatMap((response) -> this.readTokenResponse(response))
+                                .onErrorMap((ex) -> new OAuth2AuthenticationException(ex.getMessage())))
+                        .switchIfEmpty(Mono.error(() -> new OAuth2AuthenticationException("")))
+                        .flatMap((tokenResponse) -> {
+                            this.withBearerHeader(exchange, tokenResponse);
+                            return chain.filter(exchange);
+                        })
+                        .onErrorResume(AuthenticationException.class, (ex) -> this.onAuthenticationFailure(exchange));
+            }
+            return chain.filter(exchange);
+        };
+    }
+
+    protected Mono<Void> onAuthenticationFailure(ServerWebExchange exchange) {
+        return Mono.defer(() -> {
+            HttpStatus status = HttpStatus.UNAUTHORIZED;
+            ServerHttpResponse response = exchange.getResponse();
+            response.setStatusCode(status);
+            return response.setComplete();
+        });
+    }
+
+    private BodyInserters.FormInserter<String> createTokenRequestBody(String refreshToken) {
+        //TODO 更改client-id和client-secret
+        MultiValueMap<String, String> parameters = new LinkedMultiValueMap<>();
+        parameters.add(OAuth2ParameterNames.GRANT_TYPE, "refresh_token");
+        parameters.add(OAuth2ParameterNames.CLIENT_ID, "relive-client");
+        parameters.add(OAuth2ParameterNames.CLIENT_SECRET, "relive-client");
+        parameters.add(OAuth2ParameterNames.REFRESH_TOKEN, refreshToken);
+        parameters.add(OAuth2ParameterNames.SCOPE, "profile");
+        return BodyInserters.fromFormData(parameters);
+    }
+
+    private Mono<Map<String, Object>> readTokenResponse(ClientResponse response) {
+        return response.body(this.bodyExtractor);
+    }
+
+    protected void withBearerHeader(ServerWebExchange exchange, Map<String, Object> tokenResponse) {
+        exchange.mutate().request((r) -> {
+            r.headers((headers) -> {
+                headers.setBearerAuth((String) tokenResponse.get("access_token"));
+            });
+        }).build();
+
+    }
+
+}
---
+++ gateway/src/main/java/io/kp45/filter/conver/RefreshTokenAuthenticationConverter.java
@@ -0,0 +1,43 @@
+package io.kp45.filter.conver;
+
+
+import org.springframework.http.HttpCookie;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.util.CollectionUtils;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+public class RefreshTokenAuthenticationConverter implements ServerAuthenticationConverter {
+    @Override
+    public Mono<String> convert(ServerWebExchange exchange) {
+        return Mono.fromCallable(() -> token(exchange.getRequest())).map((token) -> {
+            if (token.isEmpty()) {
+                throw new OAuth2AuthenticationException("Not Found Refresh Token");
+            }
+            return token;
+        });
+    }
+
+    private String token(ServerHttpRequest request) {
+        String refreshTokenFromCookie = resolveRefreshTokenFromCookie(request);
+        if (refreshTokenFromCookie != null) {
+            return refreshTokenFromCookie;
+        }
+
+        return null;
+    }
+
+    private static String resolveRefreshTokenFromCookie(ServerHttpRequest request) {
+        MultiValueMap<String, HttpCookie> cookies = request.getCookies();
+        if (CollectionUtils.isEmpty(cookies)) {
+            return null;
+        }
+        HttpCookie accessToken = cookies.getFirst("REFRESH-TOKEN");
+        if (accessToken == null) {
+            return null;
+        }
+        return accessToken.getValue();
+    }
+}
