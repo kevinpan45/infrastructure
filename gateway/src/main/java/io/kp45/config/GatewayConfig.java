658a1512f36f23e5d8991326ac7910a76ca90092
relive <relive27@163.com>
2022-06-07 20:23:48 +0800

    Update WebClient enable RetryableLoadBalancerExchangeFilterFunction

--- gateway/src/main/java/io/kp45/config/GatewayConfig.java
+++ gateway/src/main/java/io/kp45/config/GatewayConfig.java
@@ -1,19 +1,16 @@
 package io.kp45.config;
 
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.loadbalancer.reactive.ReactorLoadBalancerExchangeFilterFunction;
+import org.springframework.cloud.client.loadbalancer.reactive.RetryableLoadBalancerExchangeFilterFunction;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class GatewayConfig {
-    @Autowired
-    ReactorLoadBalancerExchangeFilterFunction function;
 
     @Bean
-    public WebClient loadWebClient() {
+    public WebClient loadWebClient(final RetryableLoadBalancerExchangeFilterFunction function) {
         return WebClient.builder()
                 .filter(function).build();
     }
