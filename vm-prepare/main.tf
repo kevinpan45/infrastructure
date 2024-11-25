c09914c2e6d78a14ef3ca8ecdd09cc3549543099
KP45 <kevinpan45@163.com>
2024-11-25 11:25:20 +0800

    format terraform script

--- vm-prepare/main.tf
+++ vm-prepare/main.tf
@@ -2,16 +2,16 @@
 resource "null_resource" "init_server" {
   connection {
     type     = "ssh"
-    host     = "${host_ip}"
-    user     = "${ssh_user}"
-    password = "${ssh_password}"
+    host     = host_ip
+    user     = ssh_user
+    password = ssh_password
   }
-  
+
   provisioner "remote-exec" {
     inline = [
       "apt-get update",
       "apt-get install -y --no-install-recommends vim git zip unzip wget",
-      "curl -fsSL https://get.docker.com | bash -s docker --mirror Aliyun"
+      "curl -fsSL https://get.docker.com | bash -s docker --mirror Aliyun",
       "curl -sfL https://rancher-mirror.oss-cn-beijing.aliyuncs.com/k3s/k3s-install.sh | INSTALL_K3S_MIRROR=cn sh -s - --docker"
     ]
   }
