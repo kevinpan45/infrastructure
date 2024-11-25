033683a4b3071c9cfd4f72ab9306e2f00542b9c5
KP45 <kevinpan45@163.com>
2024-11-25 13:51:27 +0800

    update vm setup script

--- vm-prepare/setup.sh
+++ vm-prepare/setup.sh
@@ -8,20 +8,12 @@ apt-get install -y cockpit
 # Install Docker
 curl -fsSL https://get.docker.com | bash -s docker
 
-# Install Helm
-curl https://baltocdn.com/helm/signing.asc | apt-key add - &&
-    apt-get install apt-transport-https --yes &&
-    echo "deb https://baltocdn.com/helm/stable/debian/ all main" | tee /etc/apt/sources.list.d/helm-stable-debian.list &&
-    apt-get update &&
-    apt-get install helm
-
-# Install Kubectl
-curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.22.4/bin/linux/amd64/kubectl &&
-    chmod +x ./kubectl &&
-    mv ./kubectl /usr/local/bin/kubectl
+# Install k3d and create k3s cluster
+curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
+k3d cluster create local-stack
 
 # Install HashiCorp Products
-RUN apt-get install gpg coreutils &&
+apt-get install gpg coreutils &&
     wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg &&
     gpg --no-default-keyring --keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg --fingerprint &&
     curl -fsSL https://apt.releases.hashicorp.com/gpg | apt-key add - &&
